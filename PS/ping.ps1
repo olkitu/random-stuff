1a06c232dd1d3931896d15e7eaed9eb8efef6aa1
Minh Truong <olkitu@users.noreply.github.com>
2016-11-16 12:04:35 +0200

    Create ping.ps1

---
+++ PS/ping.ps1
@@ -0,0 +1,21 @@
+$destination = Read-Host "Specify destination host name or IP address "
+$datetime = get-date -f yyyy-MM-dd
+$logonly="Y"
+$ScriptDir = Split-Path $script:MyInvocation.MyCommand.Path
+$filelocation="$ScriptDir"+"\"+$destination+"_ping_results_"+$datetime+".txt"
+
+Do{
+    If($logonly -notmatch '^[yn]$' ) { Write-Warning "Invalid Entry" }
+    $logonly = Read-Host "Log to a file and Output to console? (Y/N) "
+} While($logonly -notmatch '^[yn]$')
+
+Write-Host "Default ping vaules: -count 999999 -delay 2"
+
+if ($logonly -eq "y") { 
+    write-host "Pinging host:"$destination". Log file location: "$filelocation -BackgroundColor Yellow -ForegroundColor Red
+    test-connection $destination -count 999999 -delay 2 -Verbose | format-table @{n='TimeStamp';e={Get-Date}},@{Expression={$_.Address};Label='Destination'},IPV4aDDRESS,ResponseTime | tee-object -filepath $filelocation -Append 
+}
+
+else { 
+    test-connection $destination -count 999999 -delay 2 -Verbose | format-table @{n='TimeStamp';e={Get-Date}},@{Expression={$_.Address};Label="Destination"},IPV4aDDRESS,ResponseTime 
+}
